3a5559082600ca52f555cac47daa92de78383fc9
tony-edu <136765277+tony-edu@users.noreply.github.com>
2023-06-16 15:11:45 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://zh.wikipedia.org/";//your website url
+  let target = "https://www.google.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       // rewrite request path `/backend`
       //  /backend/user/login => http://google.com/user/login
-         "^/wiki/": "/",
+         "^/backend": "/",
     },
   })(req, res);
 };
